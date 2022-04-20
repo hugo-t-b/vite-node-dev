b57d9320a1b24dd8b608dd0384f67ca52e3689bc
Hugo Brosnahan <101022007+hugo-t-b@users.noreply.github.com>
2022-04-20 18:57:01 +1000

    test: Add test for error handler

---
+++ tests/error-handler.test.ts
@@ -0,0 +1,75 @@
+import { afterEach, describe, expect, it, vi } from "vitest";
+
+import errorHandler from "../src/error-handler";
+import { AppError } from "../src/error-handler";
+
+describe("Error handler function", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it("Logs the message to the console", () => {
+    const spy = vi.spyOn(console, "log").mockImplementation(() => undefined);
+
+    errorHandler();
+    errorHandler("");
+    errorHandler(1);
+    errorHandler(true);
+    errorHandler([]);
+    errorHandler(new Error);
+    errorHandler(null);
+
+    expect(spy).toHaveBeenCalledTimes(7);
+  });
+
+  it("Exits the process when requested", () => {
+    vi.spyOn(console, "log").mockImplementation(() => undefined);
+    const spy = vi.spyOn(process, "exit").mockImplementation(() => undefined);
+    
+    errorHandler();
+    expect(spy).not.toHaveBeenCalled();
+
+    errorHandler(1);
+    expect(spy).not.toHaveBeenCalled();
+    
+    errorHandler("");
+    expect(spy).not.toHaveBeenCalled();
+
+    errorHandler(new Error(""));
+    expect(spy).not.toHaveBeenCalled();
+
+    errorHandler(new AppError(""));
+    expect(spy).not.toHaveBeenCalled();
+
+    errorHandler({ quit: true });
+    expect(spy).not.toHaveBeenCalled();
+
+    errorHandler(new AppError("", true));
+    expect(spy).toHaveBeenCalledOnce();
+  });
+});
+
+describe("AppError class", () => {
+  it("Is an instance of Error", () => {
+    const instance = new AppError("");
+
+    expect(instance instanceof Error).toBe(true);
+    expect(instance).toHaveProperty("message");
+    expect(typeof instance.message).toBe("string");
+  });
+
+  it("Message property is set to the message", () => {
+    const { message } = new AppError("");
+    expect(message).toBe("");
+  });
+
+  it("Quit property is set correctly", () => {
+    const { quit: quitDefault } = new AppError("");
+    const { quit: quitWhenFalsePassed } = new AppError("", false);
+    const { quit: quitWhenTruePassed } = new AppError("", true);
+
+    expect(quitDefault).toBe(false);
+    expect(quitWhenFalsePassed).toBe(false);
+    expect(quitWhenTruePassed).toBe(true);
+  });
+});
