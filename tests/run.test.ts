648a564b96699278a21d7f8082f3e67724bbe684
Hugo Brosnahan <101022007+hugo-t-b@users.noreply.github.com>
2022-04-21 17:27:04 +1000

    test: Add test for run file

---
+++ tests/run.test.ts
@@ -0,0 +1,62 @@
+import { describe, expect, it, vi } from "vitest";
+import run from "../src/run";
+
+import { AppError } from "../src/error-handler";
+import { createServer } from "vite";
+
+vi.mock("vite", () => {
+  return {
+    createServer: () => {
+      return {
+        config: {
+          root: "",
+          base: ""
+        },
+
+        pluginContainer: {
+          buildStart: () => undefined
+        }
+      };
+    }
+  };
+});
+
+const executeFileSpy = vi.fn();
+
+vi.mock("vite-node/client", () => {
+  return {
+    ViteNodeRunner: vi.fn(() => {
+      return {
+        executeFile: executeFileSpy
+      };
+    })
+  };
+});
+
+vi.mock("vite-node/server", () => {
+  return {
+    ViteNodeServer: vi.fn(() => {
+      return {
+        fetchModule: () => undefined,
+        resolveId: () => undefined
+      };
+    })
+  };
+});
+
+describe("Run function", () => {
+  it("Throws when the script is not specified", async () => {
+    await expect(run).rejects.toBeInstanceOf(AppError);
+    await expect(run).rejects.toHaveProperty("quit", true);
+  });
+
+  it("Runs the file with vite-node", async () => {
+    const server = await createServer();
+    const buildStartSpy = vi.spyOn(server.pluginContainer, "buildStart");
+
+    await run(server, "index.js");
+
+    expect(buildStartSpy).toHaveBeenCalledOnce();
+    expect(executeFileSpy).toHaveBeenNthCalledWith(1, "index.js");
+  });
+});
