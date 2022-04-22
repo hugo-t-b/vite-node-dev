41b5d11a95826aad21af7b2560d72dcfd132da6d
Hugo Brosnahan <101022007+hugo-t-b@users.noreply.github.com>
2022-04-22 15:30:21 +1000

    style: Use void(0) instead of undefined

--- tests/run.test.ts
+++ tests/run.test.ts
@@ -14,7 +14,7 @@ vi.mock("vite", () => {
         },
 
         pluginContainer: {
-          buildStart: () => undefined
+          buildStart: () => void(0)
         }
       };
     }
@@ -37,8 +37,8 @@ vi.mock("vite-node/server", () => {
   return {
     ViteNodeServer: vi.fn(() => {
       return {
-        fetchModule: () => undefined,
-        resolveId: () => undefined
+        fetchModule: () => void(0),
+        resolveId: () => void(0)
       };
     })
   };
