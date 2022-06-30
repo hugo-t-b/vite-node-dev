8c78ddea330987a5a5ad8787d2b64d85a8fcbf80
Hugo Brosnahan <101022007+hugo-t-b@users.noreply.github.com>
2022-06-30 15:10:57 +1000

    feat: add run flag

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@
 
 import { program } from "commander";
 
+import type { CLIOptions } from "./types.js";
 import errorHandler from "./error-handler.js";
 import listenForInput from "./input.js";
 import runScript, { createViteNodeRunner, createViteNodeServer, createViteServer } from "./run.js";
 
-const main = async (script: string) => {
+const main = async (script: string, options: CLIOptions) => {
   const viteServer = await createViteServer();
   const viteNodeServer = createViteNodeServer(viteServer);
   let viteNodeRunner = createViteNodeRunner(viteServer, viteNodeServer);
@@ -18,6 +19,11 @@ const main = async (script: string) => {
       errorHandler(error);
     }
   };
+
+  if (options.run) {
+    await run();
+    process.exit();
+  }
 
   const rerun = () => {
     console.clear();
@@ -39,6 +45,7 @@ program.name("vite-node-dev");
 
 program
   .argument("<file>")
+  .option("-r, --run")
   .action(main);
 
 program.parse();
